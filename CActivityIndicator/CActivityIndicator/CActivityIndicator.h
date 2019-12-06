7ad9146a85c486b274c3cb0765842e540b9894c1
Bibin Tom Joseph <bibin@hifx.co.in>
2019-12-06 14:09:21 +0530

    Initial Commit

---
+++ CActivityIndicator/CActivityIndicator/CActivityIndicator.h
@@ -0,0 +1,19 @@
+//
+//  CActivityIndicator.h
+//  CActivityIndicator
+//
+//  Created by Bibin on 06/12/19.
+//  Copyright © 2019 Bibin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CActivityIndicator.
+FOUNDATION_EXPORT double CActivityIndicatorVersionNumber;
+
+//! Project version string for CActivityIndicator.
+FOUNDATION_EXPORT const unsigned char CActivityIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CActivityIndicator/PublicHeader.h>
+
+
